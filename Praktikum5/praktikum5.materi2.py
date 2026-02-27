9d2bd2807ee3aa4d9c539d5c72d0d71b9eb2da67
asha <whosnatasha@apps.ipb.ac.id>
2026-02-27 14:53:00 +0700

    Add files via upload

---
+++ Praktikum5/praktikum5.materi2.py
@@ -0,0 +1,14 @@
+# ========================================================== 
+# Contoh Rekursi 2: Tracing Masuk/Keluar 
+# ========================================================== 
+
+def hitung(n): # Base case 
+    if n == 0: 
+        print("Selesai") 
+        return 
+    print("Masuk:", n)      # fase stacking 
+    hitung(n - 1)           # pemanggilan rekursif 
+    print("Keluar:", n)     # fase unwinding 
+    
+    
+hitung(3) 
