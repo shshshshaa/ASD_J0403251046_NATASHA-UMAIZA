b6dd75e3c52f8fe5ed28c6b943c39c6429ef6435
asha <whosnatasha@apps.ipb.ac.id>
2026-03-06 15:04:07 +0700

    Delete Praktikum5/praktikum5.materi1..py

--- Praktikum5/praktikum5.materi1..py
+++
@@ -1,9 +0,0 @@
-# ========================================================== 
-# Contoh Rekursi 1: Faktorial 
-# ========================================================== 
-
-def faktorial(n): # Base case: berhenti ketika n = 0 
-    if n == 0: 
-        return 1 # Recursive case: masalah diperkecil menjadi faktorial(n-1) 
-    return n * faktorial(n - 1) 
-print(faktorial(5)) 
